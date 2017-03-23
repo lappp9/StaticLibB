1834a154cca85ccefb7c67836084e2a90b3555bf
Luke <lappp9@gmail.com>
2017-03-23 14:18:58 -0700

    Initial Commit

---
+++ StaticLibB/StaticLibB.m
@@ -0,0 +1,13 @@
+//
+//  StaticLibB.m
+//  StaticLibB
+//
+//  Created by Luke Parham on 3/23/17.
+//  Copyright © 2017 Fyusion, Inc. All rights reserved.
+//
+
+#import "StaticLibB.h"
+
+@implementation StaticLibB
+
+@end
